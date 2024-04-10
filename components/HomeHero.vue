64a23090959318bbfe69bb3a3b7c377d387372f8
Ebraheem Alhetari <hetari4py@gmail.com>
2024-04-10 04:55:48 +0300

    add Lenis

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -5,7 +5,6 @@
     <h1 class="text-body-large text-center">
       Productivity that flows <br />with your life.
     </h1>
-
     <p class="text-body-normal md:max-w-xl m-auto">
       Unleash your potential by harmonizing your daily tasks and habits with
       your unique energy rhythms.
@@ -15,7 +14,7 @@
       <Button />
     </div>
 
-    <picture class="mx-1 md:mx-10">
+    <picture class="relative mx-1 md:mx-10">
       <source
         media="(min-width: 640px)"
         srcset="~/assets/images/image10-large.png"
@@ -24,8 +23,15 @@
       <img
         src="~/assets/images/image10-small.png"
         alt="Home hero image"
-        class="w-full"
+        class="relative w-full z-20"
       />
+      <div
+        class="absolute -top-14 md:-top-12 left-0 md:left-10 bg-gradient-to-bl from-purple-700 to-cyan-900 size-32 md:size-72 blur-3xl opacity-80 md:opacity-60 z-10"
+      ></div>
+
+      <!-- <div
+        class="absolute -bottom-10 md:-bottom-32 right-0 bg-gradient-to-br from-purple-700 to-cyan-900 size-40 md:size-96 blur-3xl opacity-80 md:opacity-40 z-10"
+      ></div> -->
     </picture>
   </main>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -9,4 +9,18 @@
   />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Lenis from "@studio-freight/lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+const lenis = new Lenis();
+
+lenis.on("scroll", ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
+</script>
