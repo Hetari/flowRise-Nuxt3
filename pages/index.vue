6d467b92a8911f013c1e1a9020d0fe55a6ec2043
Ebraheem Alhetari <hetari4py@gmail.com>
2024-04-10 16:35:17 +0300

    add animation

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,27 @@
   <main class="max-w-md md:max-w-full m-auto p-12 text-center space-y-36">
     <HomeHero />
 
-    <UsersSaid />
+    <UsersSaid class="opacity-0 translate-y-20" id="users-said" />
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+onMounted(() => {
+  gsap.to("#users-said", {
+    opacity: 1,
+    y: 0,
+    scrollTrigger: {
+      trigger: "#users-said",
+      start: "top 70%",
+      end: "bottom 50%",
+      scrub: true,
+    },
+    duration: 1,
+    ease: "circ.out",
+  });
+});
+</script>
