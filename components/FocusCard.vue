7eaaf92be55bda647a641ff7e7174a9e38599a82
Ebraheem Alhetari <hetari4py@gmail.com>
2024-04-13 00:05:14 +0300

    finish the focus card section

--- components/FocusCard.vue
+++ components/FocusCard.vue
@@ -1,10 +1,19 @@
 <template>
   <section
-    class="w-[345px] text-center space-y-4 md:space-y-0 md:w-full md:flex md:even:flex-row-reverse md:items-center md:justify-evenly md:gap-36 md:pb-56 md:pt-32"
+    :id="`card${index}`"
+    class="w-[345px] text-center space-y-4 md:space-y-0 md:w-full md:flex md:even:flex-row-reverse md:items-center md:justify-around md:gap-36 md:pb-56 md:pt-32"
   >
-    <!-- :src="`~/assets/images/${card.image}`" -->
-    <img :src="cardImage" alt="relaxing woman" class="w-full md:w-2/4" />
-    <div class="text-start space-y-2 max-w-sm">
+    <div
+      :id="`image${index}`"
+      class="opacity-0 group w-full md:w-2/4 rounded-2xl overflow-hidden group/item shadow-lg"
+    >
+      <img
+        :src="cardImage"
+        alt="relaxing woman"
+        class="w-full scale-110 group-hover:scale-100 transition-all duration-300"
+      />
+    </div>
+    <div :id="`text${index}`" class="text-start space-y-2 max-w-sm opacity-0">
       <h3>{{ card.title }}</h3>
       <p class="text-body-small">
         {{ card.body }}
@@ -14,6 +23,9 @@
 </template>
 
 <script setup lang="ts">
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
 const props = defineProps<{
   card: {
     id: number;
@@ -21,9 +33,51 @@ const props = defineProps<{
     body: string;
     image: string;
   };
+  index: number;
+  pos: number;
 }>();
 
 const cardImage = computed(() => {
   return "_nuxt/assets/images/" + props.card.image;
+});
+
+// animation
+
+onMounted(() => {
+  gsap.set(`#image${props.index}`, {
+    xPercent: props.pos,
+  });
+
+  gsap.set(`#text${props.index}`, {
+    xPercent: props.pos * -1,
+  });
+
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.to(`#image${props.index}`, {
+    xPercent: 0,
+    opacity: 1,
+    scrollTrigger: {
+      trigger: `#card${props.index}`,
+      start: "top 75%",
+      end: "bottom 97%",
+      scrub: true,
+    },
+    duration: 1,
+    ease: "power4.inOut",
+  });
+
+  gsap.to(`#text${props.index}`, {
+    xPercent: 0,
+    opacity: 1,
+    scrollTrigger: {
+      trigger: `#card${props.index}`,
+      start: "top 75%",
+      end: "bottom 97%",
+      scrub: true,
+      //   markers: true,
+    },
+    duration: 1,
+    ease: "power4.inOut",
+  });
 });
 </script>
--- components/FocusSection.vue
+++ components/FocusSection.vue
@@ -1,6 +1,12 @@
 <template>
   <section class="grid justify-items-center gap-10 max-w-[1400px] m-auto">
-    <FocusCard v-for="card in FocusCardInfo" :key="card.id" :card="card" />
+    <FocusCard
+      v-for="(card, index) in FocusCardInfo"
+      :key="card.id"
+      :card="card"
+      :index="index"
+      :pos="index % 2 === 0 ? -100 : 100"
+    />
   </section>
 </template>
 
